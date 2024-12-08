a49d76b1b1c4e13d74bb92e6dd71a41af455fa0f
switch23 <switch153r81953@icloud.com>
2024-12-08 15:11:19 +0900

    templeteをqueryに変更

--- image-rec/test.py
+++ image-rec/test.py
@@ -3,8 +3,8 @@ import cv2
 import image_rec
 
 # テンプレート画像
-tmplete_img = cv2.imread("./src/templete_arrow.png", 0)
-tmp_kp, tmp_des = image_rec.create_vec(tmplete_img)
+query_img = cv2.imread("./src/templete_arrow.png", 0)
+query_kp, query_des = image_rec.create_vec(query_img)
 
 # 入力画像
 input_img_list = []
@@ -18,10 +18,10 @@ input_img_list.append(cv2.imread("./src/drow_img.png", 0))
 for input_img in input_img_list:
     # メインロジック
     input_kp, input_des = image_rec.create_vec(input_img)
-    matches = image_rec.knn_match(tmp_des, input_des)
+    matches = image_rec.knn_match(query_des, input_des)
     good = image_rec.select_good_matches(matches, 0.2)
     # 目視確認
-    result_img = cv2.drawMatchesKnn(tmplete_img, tmp_kp, input_img, input_kp, good, None, flags=2)
+    result_img = cv2.drawMatchesKnn(query_img, query_kp, input_img, input_kp, good, None, flags=2)
     cv2.imshow('result_img', result_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
