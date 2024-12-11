92256bf6a08b1dc870b9b901dbe13f639622be1f
switch23 <switch153r81953@icloud.com>
2024-12-11 22:55:05 +0900

    picamera2をcapture_file に変更

--- Code/Server/camera.py
+++ Code/Server/camera.py
@@ -1,3 +1,11 @@
 from picamera2 import Picamera2
 picam2 = Picamera2()
-picam2.start_and_capture_file("image.jpg")
+
+# こいつはデフォだがバグる
+# picam2.start_and_capture_file("image.jpg")
+
+picam2.start()
+
+picam2.capture_file("capture_image.jpg")
+
+picam2.stop()
